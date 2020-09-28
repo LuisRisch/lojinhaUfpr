7057e5f63145cd0953da6a542812d79dd1be74c8
Luis Felipe Risch <lfr20@inf.ufpr.br>
2020-09-28 12:59:54 -0300

    Estilo do Arquivo BoxProduct.js

---
+++ Components/CustomBoxesProductInformation/Styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'; 
+import Spacing from '../../Constants/Spacing';
+
+const Styles = StyleSheet.create({
+    box_container : {
+        marginTop : Spacing.MainMargin, 
+        justifyContent : 'flex-start',
+    }
+}) 
+
+export { Styles }; 
