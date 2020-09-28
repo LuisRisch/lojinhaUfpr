f8bf5f265da93b6c5e26fa20e4868288dc9c474a
Luis Felipe Risch <lfr20@inf.ufpr.br>
2020-09-28 13:03:12 -0300

    Estilo do arquivo CustomSubLabel.js

---
+++ Components/CustomSubLabel/Styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+import Colors from '../../Constants/Colors'; 
+import FontSize from '../../Constants/FontSizes'; 
+
+const Styles = StyleSheet.create({
+    text_style : {
+        color : Colors.lightGrey, 
+        fontWeight : '500', 
+        fontSize : FontSize.Lower
+    }
+}) 
+
+export {Styles}
