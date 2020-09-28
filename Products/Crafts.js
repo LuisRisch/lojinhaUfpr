2d311e651e3ddaea6c3bc031d63ae1ca0b8632ca
Luis Felipe Risch <lfr20@inf.ufpr.br>
2020-09-28 18:37:32 -0300

    Arquivo temporaria para teste de dar display em categorias diferentes

---
+++ Products/Crafts.js
@@ -0,0 +1,27 @@
+var product5 = {
+    Title: "Vaso de flores feito em casa!",
+    Description:
+        "Lindo vaso de flores, em que o vaso feito foi á mão. As flores provém do exterior",
+    Category: "Artesanato",
+    Price: "34.4",
+    AnnouncedBy: "Maria Noga Risch",
+    imgUrl:
+        "https://firebasestorage.googleapis.com/v0/b/lojinhaufpr.appspot.com/o/artesanato.jpg?alt=media&token=e2cb9924-0e7f-4d84-83cd-2265e236fdc0",
+    id: "5",
+};
+
+var product3 = {
+    Title: "Vaso de flores feito em casa!",
+    Description:
+        "Lindo vaso de flores, em que o vaso feito foi á mão. As flores provém do exterior",
+    Category: "Artesanato",
+    Price: "34.4",
+    AnnouncedBy: "Maria Noga Risch",
+    imgUrl:
+        "https://firebasestorage.googleapis.com/v0/b/lojinhaufpr.appspot.com/o/artesanato.jpg?alt=media&token=e2cb9924-0e7f-4d84-83cd-2265e236fdc0",
+    id: "3",
+}; 
+
+var ListOfCrafts = [product5 , product3]; 
+
+export { ListOfCrafts };
