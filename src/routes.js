cf8b9fe2c4d79df775ec268129655cd67db40cd6
Luis Felipe Risch <lfr20@inf.ufpr.br>
2020-10-15 18:47:35 -0300

    Rotas para registro de ufpr

--- src/routes.js
+++ src/routes.js
@@ -29,141 +29,169 @@ const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
 const StackRoutes = ({ navigation }) => (
-  <Stack.Navigator
-    initialRouteName="MainProducts"
-    screenOptions={{ headerShown: false }}
-  >
-    <Stack.Screen name="MainProducts" component={MainProducts} />
-    <Stack.Screen
-      name="ProductScreen"
-      component={ProductScreen}
-      options={{
-        headerStyle: {
-          backgroundColor: Colors.backgroundWhite,
-          elevation: 0,
-        },
-        headerShown: true,
-        title: "",
-        headerLeft: () => (
-          <TouchableOpacity onPress={() => navigation.openDrawer()}>
-            <Icon
-              name="align-left"
-              size={20}
-              color="#c4c4c4"
-              style={{ marginLeft: 18 }}
-            />
-          </TouchableOpacity>
-        ),
-        headerRight: () => (
-          <TouchableOpacity
-            onPress={() => navigation.navigate("SearchProduct")}
-          >
-            <Icon
-              name="search"
-              size={20}
-              color="#c4c4c4"
-              style={{ marginRight: 18 }}
-            />
-          </TouchableOpacity>
-        ),
-      }}
-    />
-    <Stack.Screen
-      name="ChatScreen"
-      component={ChatScreen}
-      options={{ headerShown: true, title: "" }}
-    />
-    {/* <Stack.Screen
-      name="Register"
-      component={Register}
-      options={{ title: "Anunciar produto/serviço" }}
-    /> */}
-    <Stack.Screen 
-        name="ConfirmAnnouncement" 
-        component={ConfirmAnnouncement} 
-    />
-    <Stack.Screen 
-        name="SearchProduct" 
-        component={SearchProduct} 
-    />
-    <Stack.Screen 
-        name="Register" 
-        component={Register} 
-    /> 
-    <Stack.Screen 
-        name="UfprRegister" 
-        component={UfprRegister} 
-    /> 
-    <Stack.Screen 
-        name="FinishUfprRegister" 
-        component={FinishUfprRegister} 
-    />
-  </Stack.Navigator>
+    <Stack.Navigator
+        initialRouteName="MainProducts"
+        screenOptions={{ headerShown: false }}
+    >
+        <Stack.Screen
+            name="MainProducts"
+            component={MainProducts}
+            options={{
+                headerStyle: {
+                    backgroundColor: Colors.backgroundWhite,
+                    elevation: 0,
+                },
+                headerShown: true,
+                title: "",
+                headerLeft: () => (
+                    <TouchableOpacity onPress={() => navigation.openDrawer()}>
+                        <Icon
+                            name="align-left"
+                            size={20}
+                            color="#c4c4c4"
+                            style={{ marginLeft: 18 }}
+                        />
+                    </TouchableOpacity>
+                ),
+                headerRight: () => (
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate("SearchProduct")}
+                    >
+                        <Icon
+                            name="search"
+                            size={20}
+                            color="#c4c4c4"
+                            style={{ marginRight: 18 }}
+                        />
+                    </TouchableOpacity>
+                ),
+            }}
+        />
+        <Stack.Screen
+            name="ProductScreen"
+            component={ProductScreen}
+            options={{
+                headerStyle: {
+                    backgroundColor: Colors.backgroundWhite,
+                    elevation: 0,
+                },
+                headerShown: true,
+                title: "",
+                headerLeft: () => (
+                    <TouchableOpacity onPress={() => navigation.openDrawer()}>
+                        <Icon
+                            name="align-left"
+                            size={20}
+                            color="#c4c4c4"
+                            style={{ marginLeft: 18 }}
+                        />
+                    </TouchableOpacity>
+                ),
+                headerRight: () => (
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate("SearchProduct")}
+                    >
+                        <Icon
+                            name="search"
+                            size={20}
+                            color="#c4c4c4"
+                            style={{ marginRight: 18 }}
+                        />
+                    </TouchableOpacity>
+                ),
+            }}
+        />
+        <Stack.Screen
+            name="ChatScreen"
+            component={ChatScreen}
+            options={{ headerShown: true, title: "" }}
+        />
+        <Stack.Screen
+            name="ConfirmAnnouncement"
+            component={ConfirmAnnouncement}
+        />
+        <Stack.Screen
+            name="SearchProduct"
+            component={SearchProduct}
+        />
+        <Stack.Screen
+            name="Register"
+            component={Register}
+        />
+        <Stack.Screen
+            name="UfprRegister"
+            component={UfprRegister}
+        />
+        <Stack.Screen
+            name="FinishUfprRegister"
+            component={FinishUfprRegister}
+        />
+    </Stack.Navigator>
 );
 
 function Routes() {
-  const user = useSelector((state) => state.user.data);
+    const user = useSelector((state) => state.user.data);
 
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  return (
-    <Drawer.Navigator
-      initialRouteName="Login"
-      drawerContent={(props) => <CustomDrawer {...props} />}
-      drawerContentOptions={{
-        activeTintColor: "#ed524a",
-        labelStyle: {
-          fontWeight: "bold",
-        },
-      }}
-    >
-      <Drawer.Screen
-        name="UserPage"
-        component={UserPage}
-        options={{ title: "Sua conta" }}
-      />
-      <Drawer.Screen
-        name="MainProducts"
-        component={StackRoutes}
-        options={{ title: "Loja" }}
-      />
-      <Drawer.Screen
-        name="ChatList"
-        component={ChatList}
-        options={{ title: "Chat" }}
-      />
-      <Drawer.Screen
-        name="Register"
-        component={Register}
-        options={{ title: "Register" }}
-      /> 
-      <Drawer.Screen
-        name="UfprRegister"
-        component={UfprRegister}
-        options={{ title: "UfprRegister" }}
-      />
-      <Drawer.Screen
-        name="FinishUfprRegister"
-        component={FinishUfprRegister}
-        options={{ title: "FinishUfprRegister" }}
-      />
-      <Drawer.Screen
-        name="CreateAnnouncement"
-        component={CreateAnnouncement}
-        options={{ title: "Anunciar produto/serviço" }}
-      />
-      <Drawer.Screen
-        name="Login"
-        component={Home}
-        options={{ title: "Sair", gestureEnabled: false }}
-        listeners={({ navigation, route }) => ({
-          // focus: (e) => {
-          //   dispatch(userSignOut());
-          // },
-        })}
-      />
-    </Drawer.Navigator>
-  );
+    return (
+        <Drawer.Navigator
+            initialRouteName="Login"
+            drawerContent={(props) => <CustomDrawer {...props} />}
+            drawerContentOptions={{
+                activeTintColor: "#ed524a",
+                labelStyle: {
+                    fontWeight: "bold",
+                },
+            }}
+        >
+            <Drawer.Screen
+                name="UserPage"
+                component={UserPage}
+                options={{ title: "Sua conta" }}
+            />
+            <Drawer.Screen
+                name="MainProducts"
+                component={StackRoutes}
+                options={{ title: "Loja" }}
+            />
+            <Drawer.Screen
+                name="ChatList"
+                component={ChatList}
+                options={{ title: "Chat" }}
+            />
+            {/* <Drawer.Screen
+                name="Register"
+                component={Register}
+                options={{ title: "Register" }}
+            />
+            <Drawer.Screen
+                name="UfprRegister"
+                component={UfprRegister}
+                options={{ title: "UfprRegister" }}
+            />
+            <Drawer.Screen
+                name="FinishUfprRegister"
+                component={FinishUfprRegister}
+                options={{ title: "FinishUfprRegister" }}
+            /> */}
+            <Drawer.Screen
+                name="CreateAnnouncement"
+                component={CreateAnnouncement}
+                options={{ title: "Anunciar produto/serviço" }}
+            />
+            <Drawer.Screen
+                name="Login"
+                component={Home}
+                options={{ title: "Sair", gestureEnabled: false }}
+                listeners={({ navigation, route }) => ({
+                    // focus: (e) => {
+                    //   dispatch(userSignOut());
+                    // },
+                })}
+            />
+        </Drawer.Navigator>
+    );
 }
 
 export default Routes;
