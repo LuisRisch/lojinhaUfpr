7c7957a8d8cf4f39fc537f2e1c6f0f98a6920016
Luis Felipe Risch <lfr20@inf.ufpr.br>
2020-10-22 20:32:45 -0300

    Fontes customizadas

--- src/components/CustomSubLabel/index.js
+++ src/components/CustomSubLabel/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Text } from 'react-native'; 
 import { Styles } from './Styles'; 
 
--- src/components/CustomTopLabelInput/index.js
+++ src/components/CustomTopLabelInput/index.js
@@ -1,9 +1,31 @@
-import React from 'react'; 
+import React, { useState } from 'react'; 
 import { Text  } from 'react-native'; 
-import { styles } from './Styles'; 
+import { styles } from './Styles';  
 
-export default function CustomTopLabelInput( props ){
-    return(
-        <Text style = { styles.normalText }>{ props.label } </Text>
-    );
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+const getFonts = () => 
+  Font.loadAsync({
+    "ralway-regular": require("../../assets/fonts/Raleway-Regular.ttf"),
+    "ralway-regular-semi": require("../../assets/fonts/Raleway-SemiBold.ttf"),
+    "ralway-regular-bold": require("../../assets/fonts/Raleway-Bold.ttf"),
+    "Mplus-semi": require("../../assets/fonts/MPLUSRounded1c-Medium.ttf"),
+    "Mplus-bold": require("../../assets/fonts/MPLUSRounded1c-Bold.ttf"),
+  });
+
+
+export default function CustomTopLabelInput( props ){ 
+    const [fontsLoaded, setFontsLoaded] = useState(false); 
+    if(fontsLoaded){
+        return(
+            <Text style = { styles.normalText }>{ props.label } </Text>
+        );
+    } else {
+        return <AppLoading 
+            startAsync={getFonts} 
+            onFinish={() => setFontsLoaded(true)}
+        />
+    }
+    
 } 
