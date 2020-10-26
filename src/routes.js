d063aaec2f98be81431a87fad6d2bdae0c203c11
Luis Felipe Risch <lfr20@inf.ufpr.br>
2020-10-26 16:00:01 -0300

    criou uma rota para testar o push notifcation, mas ja foi apagada

--- src/routes.js
+++ src/routes.js
@@ -20,7 +20,8 @@ import SearchProduct from "./screens/SearchProduct";
 import Register from "./screens/Register";
 import ProductSummary from "./components/ProductSummaryModal/ProductSummar";
 import UfprRegister from "./screens/UfprRegister/Index";
-import FinishUfprRegister from "./screens/UfprRegister/FinishUfprRegister";
+import FinishUfprRegister from "./screens/UfprRegister/FinishUfprRegister"; 
+import PushNotifications from './screens/PushNotificationText';
 
 import LoadComponent from "./components/Load/LoadComponent";
 import CustomDrawer from "./components/CustomDrawer";
@@ -31,216 +32,216 @@ const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
 const LoginRoutes = ({ navigation, route }) => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  return (
-    <Stack.Navigator
-      initialRouteName="Login"
-      screenOptions={{ headerShown: false }}
-    >
-      <Stack.Screen name="Login" component={Home} />
-      <Stack.Screen
-        name="Register"
-        component={Register}
-        options={{
-          headerStyle: {
-            backgroundColor: Colors.backgroundWhite,
-            elevation: 0,
-          },
-          headerShown: true,
-          title: "Cadastro",
-          headerTitleAlign: "center",
-          headerLeft: () => (
-            <TouchableOpacity
-              onPress={() => navigation.dispatch(StackActions.popToTop())}
-            >
-              <Icon
-                name="chevron-left"
-                size={20}
-                color="#c4c4c4"
-                style={{ marginLeft: 18 }}
-              />
-            </TouchableOpacity>
-          ),
-        }}
-      />
-      <Stack.Screen name="UfprRegister" component={UfprRegister} />
-      <Stack.Screen name="FinishUfprRegister" component={FinishUfprRegister} />
-    </Stack.Navigator>
-  );
+    return (
+        <Stack.Navigator
+            initialRouteName="Login"
+            screenOptions={{ headerShown: false }}
+        >
+            <Stack.Screen name="Login" component={Home} />
+            <Stack.Screen
+                name="Register"
+                component={Register}
+                options={{
+                    headerStyle: {
+                        backgroundColor: Colors.backgroundWhite,
+                        elevation: 0,
+                    },
+                    headerShown: true,
+                    title: "Cadastro",
+                    headerTitleAlign: "center",
+                    headerLeft: () => (
+                        <TouchableOpacity
+                            onPress={() => navigation.dispatch(StackActions.popToTop())}
+                        >
+                            <Icon
+                                name="chevron-left"
+                                size={20}
+                                color="#c4c4c4"
+                                style={{ marginLeft: 18 }}
+                            />
+                        </TouchableOpacity>
+                    ),
+                }}
+            />
+            <Stack.Screen name="UfprRegister" component={UfprRegister} />
+            <Stack.Screen name="FinishUfprRegister" component={FinishUfprRegister} />
+        </Stack.Navigator>
+    );
 };
 
 const StackRoutes = ({ navigation, route }) => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  return (
-    <Stack.Navigator
-      initialRouteName="MainProducts"
-      screenOptions={{ headerShown: false }}
-    >
-      <Stack.Screen
-        name="MainProducts"
-        component={MainProducts}
-        options={{
-          headerStyle: {
-            backgroundColor: Colors.backgroundWhite,
-            elevation: 0,
-          },
-          headerShown: true,
-          title: "",
-          headerLeft: () => (
-            <TouchableOpacity onPress={() => navigation.openDrawer()}>
-              <Icon
-                name="align-left"
-                size={20}
-                color="#c4c4c4"
-                style={{ marginLeft: 18 }}
-              />
-            </TouchableOpacity>
-          ),
-          headerRight: () => (
-            <TouchableOpacity
-              onPress={() => navigation.navigate("SearchProduct")}
-            >
-              <Icon
-                name="search"
-                size={20}
-                color="#c4c4c4"
-                style={{ marginRight: 18 }}
-              />
-            </TouchableOpacity>
-          ),
-        }}
-      />
-      <Stack.Screen
-        name="ProductScreen"
-        component={ProductScreen}
-        options={{
-          headerStyle: {
-            backgroundColor: Colors.backgroundWhite,
-            elevation: 0,
-          },
-          headerShown: true,
-          title: "",
-          headerLeft: () => (
-            <TouchableOpacity onPress={() => navigation.openDrawer()}>
-              <Icon
-                name="align-left"
-                size={20}
-                color="#c4c4c4"
-                style={{ marginLeft: 18 }}
-              />
-            </TouchableOpacity>
-          ),
-          headerRight: () => (
-            <TouchableOpacity
-              onPress={() => navigation.navigate("SearchProduct")}
-            >
-              <Icon
-                name="search"
-                size={20}
-                color="#c4c4c4"
-                style={{ marginRight: 18 }}
-              />
-            </TouchableOpacity>
-          ),
-        }}
-      />
-      <Stack.Screen
-        name="ChatScreen"
-        component={ChatScreen}
-        options={({ route }) => ({
-          headerShown: true,
-          title: route.params.product,
-          headerLeft: () => (
-            <TouchableOpacity
-              onPress={() => {
-                navigation.navigate("MainProducts");
-              }}
-            >
-              <Icon
-                name="arrow-left"
-                size={20}
-                color="#c4c4c4"
-                style={{ marginLeft: 18 }}
-              />
-            </TouchableOpacity>
-          ),
-          headerRight: () => (
-            <TouchableOpacity
-              onPress={() => navigation.navigate("ProductSummary")}
-            >
-              <Image
-                source={{ uri: route.params.url }}
-                style={{
-                  height: 40,
-                  width: 40,
-                  marginRight: 18,
-                  borderRadius: 25,
+    return (
+        <Stack.Navigator
+            initialRouteName="MainProducts"
+            screenOptions={{ headerShown: false }}
+        >
+            <Stack.Screen
+                name="MainProducts"
+                component={MainProducts}
+                options={{
+                    headerStyle: {
+                        backgroundColor: Colors.backgroundWhite,
+                        elevation: 0,
+                    },
+                    headerShown: true,
+                    title: "",
+                    headerLeft: () => (
+                        <TouchableOpacity onPress={() => navigation.openDrawer()}>
+                            <Icon
+                                name="align-left"
+                                size={20}
+                                color="#c4c4c4"
+                                style={{ marginLeft: 18 }}
+                            />
+                        </TouchableOpacity>
+                    ),
+                    headerRight: () => (
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate("SearchProduct")}
+                        >
+                            <Icon
+                                name="search"
+                                size={20}
+                                color="#c4c4c4"
+                                style={{ marginRight: 18 }}
+                            />
+                        </TouchableOpacity>
+                    ),
                 }}
-              />
-            </TouchableOpacity>
-          ),
-        })}
-      />
-      <Stack.Screen
-        name="ConfirmAnnouncement"
-        component={ConfirmAnnouncement}
-      />
-      <Stack.Screen name="SearchProduct" component={SearchProduct} />
-      <Stack.Screen name="ProductSummary" component={ProductSummary} />
-    </Stack.Navigator>
-  );
+            />
+            <Stack.Screen
+                name="ProductScreen"
+                component={ProductScreen}
+                options={{
+                    headerStyle: {
+                        backgroundColor: Colors.backgroundWhite,
+                        elevation: 0,
+                    },
+                    headerShown: true,
+                    title: "",
+                    headerLeft: () => (
+                        <TouchableOpacity onPress={() => navigation.openDrawer()}>
+                            <Icon
+                                name="align-left"
+                                size={20}
+                                color="#c4c4c4"
+                                style={{ marginLeft: 18 }}
+                            />
+                        </TouchableOpacity>
+                    ),
+                    headerRight: () => (
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate("SearchProduct")}
+                        >
+                            <Icon
+                                name="search"
+                                size={20}
+                                color="#c4c4c4"
+                                style={{ marginRight: 18 }}
+                            />
+                        </TouchableOpacity>
+                    ),
+                }}
+            />
+            <Stack.Screen
+                name="ChatScreen"
+                component={ChatScreen}
+                options={({ route }) => ({
+                    headerShown: true,
+                    title: route.params.product,
+                    headerLeft: () => (
+                        <TouchableOpacity
+                            onPress={() => {
+                                navigation.navigate("MainProducts");
+                            }}
+                        >
+                            <Icon
+                                name="arrow-left"
+                                size={20}
+                                color="#c4c4c4"
+                                style={{ marginLeft: 18 }}
+                            />
+                        </TouchableOpacity>
+                    ),
+                    headerRight: () => (
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate("ProductSummary")}
+                        >
+                            <Image
+                                source={{ uri: route.params.url }}
+                                style={{
+                                    height: 40,
+                                    width: 40,
+                                    marginRight: 18,
+                                    borderRadius: 25,
+                                }}
+                            />
+                        </TouchableOpacity>
+                    ),
+                })}
+            />
+            <Stack.Screen
+                name="ConfirmAnnouncement"
+                component={ConfirmAnnouncement}
+            /> 
+            <Stack.Screen name="SearchProduct" component={SearchProduct} />
+            <Stack.Screen name="ProductSummary" component={ProductSummary} />
+        </Stack.Navigator>
+    );
 };
 
 function Routes() {
-  const user = useSelector((state) => state.user.data);
+    const user = useSelector((state) => state.user.data);
 
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  return (
-    <Drawer.Navigator
-      initialRouteName="Login"
-      drawerContent={(props) => <CustomDrawer {...props} user={user} />}
-      drawerContentOptions={{
-        activeTintColor: "#ed524a",
-        labelStyle: {
-          fontWeight: "bold",
-        },
-      }}
-    >
-      <Drawer.Screen
-        name="UserPage"
-        component={UserPage}
-        options={{ title: "Sua conta" }}
-      />
-      <Drawer.Screen
-        name="Products"
-        component={StackRoutes}
-        options={{ title: "Loja" }}
-      />
-      <Drawer.Screen
-        name="ChatList"
-        component={ChatList}
-        options={{ title: "Chat" }}
-        listeners={{
-          focus: () => {
-            dispatch(chatLeave());
-          },
-        }}
-      />
-      <Drawer.Screen
-        name="CreateAnnouncement"
-        component={CreateAnnouncement}
-        options={{ title: "Anunciar produto/serviço" }}
-      />
-      <Drawer.Screen
-        name="Login"
-        component={LoginRoutes}
-        options={{ title: "Sair", gestureEnabled: false }}
-      />
-    </Drawer.Navigator>
-  );
+    return (
+        <Drawer.Navigator
+            initialRouteName="Login"
+            drawerContent={(props) => <CustomDrawer {...props} user={user} />}
+            drawerContentOptions={{
+                activeTintColor: "#ed524a",
+                labelStyle: {
+                    fontWeight: "bold",
+                },
+            }}
+        >
+            <Drawer.Screen
+                name="UserPage"
+                component={UserPage}
+                options={{ title: "Sua conta" }}
+            />
+            <Drawer.Screen
+                name="Products"
+                component={StackRoutes}
+                options={{ title: "Loja" }}
+            />
+            <Drawer.Screen
+                name="ChatList"
+                component={ChatList}
+                options={{ title: "Chat" }}
+                listeners={{
+                    focus: () => {
+                        dispatch(chatLeave());
+                    },
+                }}
+            />
+            <Drawer.Screen
+                name="CreateAnnouncement"
+                component={CreateAnnouncement}
+                options={{ title: "Anunciar produto/serviço" }}
+            />
+            <Drawer.Screen
+                name="Login"
+                component={LoginRoutes}
+                options={{ title: "Sair", gestureEnabled: false }}
+            />
+        </Drawer.Navigator>
+    );
 }
 
 export default Routes;
